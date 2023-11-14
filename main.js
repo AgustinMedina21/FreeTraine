0e769e9df0d99168549d54fd8c1080cdee93ae67
AgustinMedina21 <agus.demian21@gmail.com>
2023-11-13 22:57:52 -0300

    Se le agrego JS y se le aplica unos alerts a los botones de servicio

---
+++ main.js
@@ -0,0 +1,19 @@
+miBoton = document.getElementById("miBotonA");
+miBoton.addEventListener("click", function() {
+    alert("Todavia no disponible. Intenta de nuevo mas tarde.");
+});
+
+miBoton = document.getElementById("miBotonH");
+miBoton.addEventListener("click", function() {
+    alert("Todavia no disponible. Intenta de nuevo mas tarde.");
+});
+
+miBoton = document.getElementById("miBotonD");
+miBoton.addEventListener("click", function() {
+    alert("Todavia no disponible. Intenta de nuevo mas tarde.");
+});
+
+miBoton = document.getElementById("miBotonR");
+miBoton.addEventListener("click", function() {
+    alert("Todavia no disponible. Intenta de nuevo mas tarde.");
+});
